2d9c70227882f1af36eaeed4dee2df1a3dfa50e7
zainabbaker290 <119340513@umail.ucc.ie>
2022-11-06 19:18:49 +0000

    interfaces rough draft

---
+++ battery.py
@@ -0,0 +1,18 @@
+class Battery:
+    def __init__(self):
+        #has a battery state 
+        pass
+    
+    def get_battery_level(self):
+        #returns battery level
+        pass
+    
+
+    #should this be a setter ? 
+    def raise_battery_level():
+        #check to see if at dock 
+        #raise battery percentage 
+        #confused here cause does this make sense 
+            #checks if at dock from sensor 
+            #if yes then charging 
+        pass
---
+++ zoomi.py
@@ -0,0 +1,26 @@
+class Zoomi:
+    def __init__(self) -> None:
+        #zoomi has battery, sensors, light , dirt compartment
+        pass
+    
+    def activate_zoomi():
+        #checks if battery low 
+            #if low = go to base 
+            #tells user zoomi is asleep 
+            #app pings zoomi until zoomi up again 
+        
+        #checks dirt compartment 
+            #If nearly full → 
+            # warns user to empty 
+            # or else zoomi might not properly clean the room → if user presses yes zoomi will hoover but might not be able to pick up anything 
+        
+        #checks if last clean was finished in total
+            #If no → goes to last location saved and starts from there
+        
+        #constantly pings battery level to see if its below 10%, if below, goes back to base 
+
+        #naviagtes around room 
+        #constantly calls on sensors 
+        #once finished return to base dock
+    
+        pass
