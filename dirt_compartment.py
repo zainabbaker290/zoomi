081dcc9f2fb471e969a85f5d4001a8e04396fa9c
zainabbaker290 <119340513@umail.ucc.ie>
2022-11-06 20:00:05 +0000

    interfaces rough draft 2

---
+++ dirt_compartment.py
@@ -0,0 +1,11 @@
+class DirtCompartment:
+    def __init__(self) -> None:
+        pass
+
+    def get_dirt_level():
+        #return dirt level 
+        pass
+
+    def warn_user():
+        #if 90% full, use this function to warn user that zoomi might not hoover well
+        pass 
