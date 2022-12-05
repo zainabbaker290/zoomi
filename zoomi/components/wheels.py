1be5bbe1906a420e1ca1afbd6841c8279aa28c45
niamhConnolly1 <120346431@umail.ucc.ie>
2022-12-05 21:16:34 +0000

    deleted files

--- zoomi/components/wheels.py
+++
@@ -1,9 +0,0 @@
-class Wheels:
-    def __init__(self):
-        self.wheels = 0
-
-    def turn_wheels(self):
-        if self.wheels == 360:
-            self.wheels = 0 
-        else:
-            self.wheels += 90 
