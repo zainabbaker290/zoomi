4af1f4c2760f54a31c4adf7b84bc3e9dee839a86
niamhConnolly1 <120346431@umail.ucc.ie>
2022-12-05 21:14:39 +0000

    modified files

--- zoomi/components/base_dock.py
+++ zoomi/components/base_dock.py
@@ -2,7 +2,3 @@ class BaseDock():
     def __init__(self):
         self.x = 0
         self.y = 0
-
-    def call_zoomi_home(self):
-        #zoomi base starts at (0,0)
-        return (self.x, self.y)
